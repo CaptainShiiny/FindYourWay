e0d7af5f5ba48a6c085e63328b0bd5ed9241015e
Barzimuto <kevinbarre474@gmail.com>
2017-02-06 11:12:54 +0100

    modif player.php

--- admin/src/models/Player.php
+++ admin/src/models/Player.php
@@ -10,4 +10,8 @@ class Player extends Model{
 	protected $fillable = ["id", "pseudo"];
 	public $timestamps = false;
 
+	public function Game(){
+		return $this->hasMany("src\models\Clue", "game_id");
+	}
+
 }
