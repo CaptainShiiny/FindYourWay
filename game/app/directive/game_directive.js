f00268beaf9f1ba05b702a436b95ef7ffc3c720e
ksakho <kordiasakho@gmail.com>
2017-02-07 13:26:58 +0100

    modif

--- game/app/directive/game_directive.js
+++ game/app/directive/game_directive.js
@@ -2,15 +2,9 @@ angular.module("findyourway").directive("game", [
     function(List){
         return{
             restrict: "E",
-            templateUrl: "app/templates/game.html"
+            templateUrl: "app/templates/game.html",
             link: function(scope, element, attrs){
-                scope.displayForm = function(){
-                    if(scope.task_name){
-                        Task.addTask(scope.task_name, scope.parent_list.id);
-                    }else{
-                        Task.addTask($("#task_name").val(), scope.parent_list.id);
-                    }
-                }
+            
             }
         };
     }
