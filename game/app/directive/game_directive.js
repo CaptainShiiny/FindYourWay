87054ff783474a3f50d5590c81777aad5c5ab23c
DORIAN-PC\DorianHa <dorian.vandenbroucke@outlook.com>
2017-02-07 10:48:27 +0200

    bootstrap

--- game/app/directive/game_directive.js
+++ game/app/directive/game_directive.js
@@ -3,6 +3,15 @@ angular.module("findyourway").directive("game", [
         return{
             restrict: "E",
             templateUrl: "app/templates/game.html"
+            link: function(scope, element, attrs){
+                scope.displayForm = function(){
+                    if(scope.task_name){
+                        Task.addTask(scope.task_name, scope.parent_list.id);
+                    }else{
+                        Task.addTask($("#task_name").val(), scope.parent_list.id);
+                    }
+                }
+            }
         };
     }
 ]);
