80631d79ef56c4f5a9e1a5ef0b653298373cf97b
DORIAN-PC\DorianHa <dorian.vandenbroucke@outlook.com>
2017-02-08 12:09:35 +0200

    affichage indications

--- game/app/directive/place_directive.js
+++ game/app/directive/place_directive.js
@@ -1,17 +1,8 @@
-angular.module("findyourway").directive("place", 
-["Place", function(Place){
-
-    return{
-        restrict: "E",
-        templateUrl: "app/templates/place.html", 
-        link: function(scope, element, attrs){
-            scope.randPlaces = function(){
-                //var randPlaces = scope.randPlaces;
-                //Place.randPlaces();
-                console.log("gg");
-            }
-            
-        }
-    };
-
-}]);
+angular.module("findyourway").directive("place", [
+    function(Player, Place){
+        return{
+            restrict: "E",
+            templateUrl: "app/templates/place.html"
+        };
+    }
+]);
