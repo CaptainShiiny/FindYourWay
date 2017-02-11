473f5034d496b99b732fb9100775d7361ebf365d
DORIAN-PC\DorianHa <dorian.vandenbroucke@outlook.com>
2017-02-11 10:47:26 +0200

    bug destination finale

--- game/app/directive/map_directive.js
+++ game/app/directive/map_directive.js
@@ -8,9 +8,10 @@ angular.module('findyourway').directive('map', [
                 scope.clickOnMap = function(){
                     if(localStorage.getItem("score") && localStorage.getItem("final_destination")){
                         alert("Félicitations, vous avez gagné !");
+                        Player.updateScore(localStorage.getItem("score"));
+                        localStorage.removeItem("score");
                         localStorage.removeItem("final_destination");
-                    }
-                    if(localStorage.getItem("score")){
+                    }else if(localStorage.getItem("score")){
                         Player.updateScore(localStorage.getItem("score"));
                         localStorage.removeItem("score");
                     }
